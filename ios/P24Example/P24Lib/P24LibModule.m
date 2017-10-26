210055b5d96970d76722bcc772da105746657845
Arek <>
2017-10-26 12:09:50 +0200

    ios fix

--- ios/P24Example/P24Lib/P24LibModule.m
+++ ios/P24Example/P24Lib/P24LibModule.m
@@ -48,7 +48,7 @@ RCT_EXPORT_METHOD(startTrnDirectWithParams:(NSDictionary*)params callback:(RCTRe
   P24TransactionParams* trasaction = [P24LibModule transactionParams:params[@"transactionParams"]];
   
   P24TrnDirectParams* trnParams = [[P24TrnDirectParams alloc] initWithTransactionParams:trasaction];
-  trnParams.sandbox = params[@"isSandbox"];
+  trnParams.sandbox = [@"true" isEqualToString:params[@"isSandbox"]];
   trnParams.settings = [P24LibModule settingsFromParams: params[@"settingsParams"]];
   
   dispatch_sync(dispatch_get_main_queue(), ^{
